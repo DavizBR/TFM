7e223ea31c49a792381a053d41988840471c2b56
David <126162659+DavizBR@users.noreply.github.com>
2023-08-30 16:06:50 +0200

    Update actions.py

--- actions.py
+++ actions.py
@@ -4,6 +4,7 @@
 # See this guide on how to implement these action:
 # https://rasa.com/docs/rasa/custom-actions
 import datetime
+from matplotlib import image
 import pytz
 import re
 import sqlite3
@@ -50,27 +51,32 @@ class ActionInformacionCuadro(Action):
                 {
                     "nombre": ["la maja vestida", "maja"],
                     "descripcion": "Este lienzo, posiblemente creado después de La Maja Desnuda, presenta a la maja recostada en un sofá verde, vestida con transparencias y detalles como un fajín rosa y chaquetilla amarilla. El cuadro muestra su destreza artística en las texturas y colores, permitiéndose una mayor libertad pictórica en comparación con su obra anterior. Los detalles del vestuario, el encarnado del rostro y el cabello negro son capturados con destreza. Su conexión con las majas como gitanas se menciona en inventarios de bienes.",
-                    "enlace": "https://fundaciongoyaenaragon.es/obra/la-maja-vestida/581"
+                    "enlace": "https://fundaciongoyaenaragon.es/obra/la-maja-vestida/581",
+                    "imagen": "https://fundaciongoyaenaragon.es/files/resize/800x600/files/images/581_7.9.14.jpg"
                 },
                 {
                     "nombre": ["los fusilamientos del tres de mayo", "fusilamientos", "tres de mayo"],
                     "descripcion": "Los Fusilamientos representan la brutalidad de la represión durante la guerra de la Independencia española. La pintura muestra condenados esperando su ejecución, rodeados de víctimas previas y soldados implacables. Las expresiones de miedo, rabia y desesperación en los condenados revelan la crueldad y la irracionalidad de la violencia en un momento oscuro de la historia.",
-                    "enlace": "https://fundaciongoyaenaragon.es/obra/el-tres-de-mayo-de-1808/181"
+                    "enlace": "https://fundaciongoyaenaragon.es/obra/el-tres-de-mayo-de-1808/181",
+                    "imagen": "https://fundaciongoyaenaragon.es/files/resize/800x600/files/images/181_6.2.8.jpg"
                 },
                 {
                     "nombre": ["saturno devorando a sus hijos", "saturno"],
                     "descripcion": "Cuadro tétrico del titán Saturno devorando a su descendencia.",
-                    "enlace": "https://fundaciongoyaenaragon.es/obra/saturno-devorando-a-un-hijo/660"
+                    "enlace": "https://fundaciongoyaenaragon.es/obra/saturno-devorando-a-un-hijo/660",
+                    "imagen": "https://fundaciongoyaenaragon.es/files/resize/800x600/files/images/saturno79327.jpg"
                 },
                 {
                     "nombre": ["el quitasol", "quitasol"],
                     "descripcion": "Cuadro costumbrista en la que un majo protege de la luz solar a una maja.",
-                    "enlace": "https://fundaciongoyaenaragon.es/obra/el-quitasol/17"
+                    "enlace": "https://fundaciongoyaenaragon.es/obra/el-quitasol/17",
+                    "imagen": "https://fundaciongoyaenaragon.es/files/resize/800x600/files/images/17_4.2.6.jpg"
                 },
                 {
                     "nombre": ["el coloso", "coloso"],
                     "descripcion": "Cuadro fantástico de un coloso amenazante en la lejanía.",
-                    "enlace": "https://fundaciongoyaenaragon.es/obra/el-coloso/589"
+                    "enlace": "https://fundaciongoyaenaragon.es/obra/el-coloso/589",
+                    "imagen": "https://fundaciongoyaenaragon.es/files/resize/800x600/files/images/589_7.3.39.jpg"
                 },
             ]
             
@@ -81,7 +87,7 @@ class ActionInformacionCuadro(Action):
                 descripcion = cuadro_info["descripcion"]
                 enlace = cuadro_info["enlace"]
 
-                respuesta = f"Autor: Francisco de Goya.\n\n{descripcion}\n\nÓleo sobre lienzo"
+                respuesta = f"Autor: Francisco de Goya.\n\n{descripcion}\n\nÓleo sobre lienzo\n\n{image}"
                 dispatcher.utter_message(text=respuesta)
             else:
                 dispatcher.utter_message(text="Lo siento, no tengo información sobre ese cuadro de Goya.")
@@ -112,21 +118,4 @@ class ActionCalcularPrecio(Action):
             dispatcher.utter_message(text="Lo siento, no pude detectar la cantidad de entradas. Prueba a expresarlo con números.")
 
         return []
-    
 
-class ActionLogs(Action):
-    def name(self) -> Text:
-        return "action_logs"
-
-    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        user_message = tracker.latest_message.get("text")
-        bot_response = dispatcher.messages[0]["text"]
-
-        db_path = "rasa.db"
-        connection = sqlite3.connect(db_path)
-        cursor = connection.cursor()
-        cursor.execute("INSERT INTO conversation_history (user_message, bot_response) VALUES (?, ?)", (user_message, bot_response))
-        connection.commit()
-        connection.close()
-
-        return []
