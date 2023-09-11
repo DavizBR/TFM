445bc0ba77a9d39c02ed392eb5fab11ec2ac3786
David <126162659+DavizBR@users.noreply.github.com>
2023-09-11 08:12:58 +0200

    Update actions.py

--- actions.py
+++ actions.py
@@ -12,6 +12,8 @@ from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import UserUtteranceReverted
+import random
+
 
 class ActionCheckMuseumOpeningHours(Action):
     def name(self) -> Text:
@@ -51,32 +53,27 @@ class ActionInformacionCuadro(Action):
                 {
                     "nombre": ["la maja vestida", "maja"],
                     "descripcion": "Este lienzo, posiblemente creado después de La Maja Desnuda, presenta a la maja recostada en un sofá verde, vestida con transparencias y detalles como un fajín rosa y chaquetilla amarilla. El cuadro muestra su destreza artística en las texturas y colores, permitiéndose una mayor libertad pictórica en comparación con su obra anterior. Los detalles del vestuario, el encarnado del rostro y el cabello negro son capturados con destreza. Su conexión con las majas como gitanas se menciona en inventarios de bienes.",
-                    "enlace": "https://fundaciongoyaenaragon.es/obra/la-maja-vestida/581",
-                    "imagen": "https://fundaciongoyaenaragon.es/files/resize/800x600/files/images/581_7.9.14.jpg"
+                    "enlace": "",
                 },
                 {
                     "nombre": ["los fusilamientos del tres de mayo", "fusilamientos", "tres de mayo"],
                     "descripcion": "Los Fusilamientos representan la brutalidad de la represión durante la guerra de la Independencia española. La pintura muestra condenados esperando su ejecución, rodeados de víctimas previas y soldados implacables. Las expresiones de miedo, rabia y desesperación en los condenados revelan la crueldad y la irracionalidad de la violencia en un momento oscuro de la historia.",
-                    "enlace": "https://fundaciongoyaenaragon.es/obra/el-tres-de-mayo-de-1808/181",
-                    "imagen": "https://fundaciongoyaenaragon.es/files/resize/800x600/files/images/181_6.2.8.jpg"
+                    "enlace": "",
                 },
                 {
                     "nombre": ["saturno devorando a sus hijos", "saturno"],
-                    "descripcion": "Cuadro tétrico del titán Saturno devorando a su descendencia.",
-                    "enlace": "https://fundaciongoyaenaragon.es/obra/saturno-devorando-a-un-hijo/660",
-                    "imagen": "https://fundaciongoyaenaragon.es/files/resize/800x600/files/images/saturno79327.jpg"
+                    "descripcion": "Este sombrío y perturbador cuadro representa un tema clásico de la mitología y simboliza la voracidad del tiempo y la destrucción inevitable. Saturno, temeroso de ser derrocado por sus hijos, los devora como una medida desesperada de autoconservación. La pintura se considera una representación cruda y aterradora del poder y la brutalidad, y se cree que refleja el contexto político y social turbulento de la época en la que Goya vivió",
+                    "enlace": "",
                 },
                 {
                     "nombre": ["el quitasol", "quitasol"],
-                    "descripcion": "Cuadro costumbrista en la que un majo protege de la luz solar a una maja.",
-                    "enlace": "https://fundaciongoyaenaragon.es/obra/el-quitasol/17",
-                    "imagen": "https://fundaciongoyaenaragon.es/files/resize/800x600/files/images/17_4.2.6.jpg"
+                    "descripcion": "La elegante pintura muestra a dos majos: una dama y su acompañante, que sostiene un quitasol. Destaca por su uso audaz de colores, la luz que se filtra a través del quitasol, iluminando el rostro de la dama, y la atmósfera rococó y costumbrista en las que se movían las clases altas del siglo XVIII.",
+                    "enlace": "",
                 },
                 {
                     "nombre": ["el coloso", "coloso"],
-                    "descripcion": "Cuadro fantástico de un coloso amenazante en la lejanía.",
-                    "enlace": "https://fundaciongoyaenaragon.es/obra/el-coloso/589",
-                    "imagen": "https://fundaciongoyaenaragon.es/files/resize/800x600/files/images/589_7.3.39.jpg"
+                    "descripcion": "Se trata de una pintura que simboliza la devastación y el caos. Representa un coloso gigante que se alza sobre una escena de destrucción, reflejando el horror de la guerra y la opresión. La figura del coloso se interpreta como un poder opresivo que domina y destruye a las personas comunes, simbolizando el sufrimiento humano en tiempos de guerra.",
+                    "enlace": "",
                 },
             ]
             
@@ -87,7 +84,7 @@ class ActionInformacionCuadro(Action):
                 descripcion = cuadro_info["descripcion"]
                 enlace = cuadro_info["enlace"]
 
-                respuesta = f"Autor: Francisco de Goya.\n\n{descripcion}\n\nÓleo sobre lienzo\n\n{image}"
+                respuesta = f"Autor: Francisco de Goya.\n\n{descripcion}\n\nÓleo sobre lienzo\n\nEsta obra no forma parte de la exposición del museo."
                 dispatcher.utter_message(text=respuesta)
             else:
                 dispatcher.utter_message(text="Lo siento, no tengo información sobre ese cuadro de Goya.")
@@ -118,4 +115,3 @@ class ActionCalcularPrecio(Action):
             dispatcher.utter_message(text="Lo siento, no pude detectar la cantidad de entradas. Prueba a expresarlo con números.")
 
         return []
-
